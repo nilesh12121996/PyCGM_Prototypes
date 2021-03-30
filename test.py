c144e070cc0209ac9bb0b5021535e1b4ff3186b4
MattGonz <mgonz1228@gmail.com>
2021-03-30 14:33:22 -0400

    add decorator_separate_params and tests

--- test.py
+++ test.py
@@ -45,3 +45,54 @@ time = timeit.timeit(
 )
 print("=======Decorator approach after modification=======")
 print(time)
+
+setup = """
+from utils import pycgmIO
+from prototypes.decorator_separate_params import gonzCGM, pyCGM
+
+data = pycgmIO.loadData('SampleData/Sample_2/RoboWalk.c3d')
+measurements = pycgmIO.loadVSK('SampleData/Sample_2/RoboSM.vsk')
+
+matt = gonzCGM(data, measurements)
+"""
+time1 = timeit.timeit(
+    '''
+matt.run()
+    ''',
+    setup=setup,
+    number=NUM_RUNS
+)
+print('\n===gonzCGM called with decorator and finding/unpacking of custom params===')
+print(time1)
+
+setup = """
+from utils import pycgmIO
+from prototypes.decorator_separate_params import gonzCGM, pyCGM
+from numpy import array, random
+
+data = pycgmIO.loadData('SampleData/Sample_2/RoboWalk.c3d')
+measurements = pycgmIO.loadVSK('SampleData/Sample_2/RoboSM.vsk')
+
+test1 = random.rand(3)
+test2 = random.rand(3)
+test3 = random.rand(3)
+test4 = random.rand(3)
+test5 = random.rand(3)
+"""
+time2 = timeit.timeit(
+    '''
+for frame in data:
+    pyCGM.calc_pelvis_joint_center(
+        pyCGM,
+        test1,
+        test2,
+        test3,
+        test4,
+        test5)
+    ''',
+    setup=setup,
+    number=NUM_RUNS
+)
+print("=======Calling function directly (with 5 random arrays)======")
+print(time2)
+print('Performance hit of the lookups: {:.2f}'.format((time1-time2)/time2*100), '%')
