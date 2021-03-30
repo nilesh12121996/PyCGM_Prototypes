7f1a2bc97d7ed234b1be8d7ca2e4d613ddfd331f
David Kim <d@vid.kim>
2021-03-29 21:15:42 -0400

    Add decorator method and tests

--- test.py
+++ test.py
@@ -15,3 +15,33 @@ time = timeit.timeit(
 )
 print("=======Original=======")
 print(time)
+
+
+setup = """
+import pycgmIO
+from decorator import CGM as decCGM
+
+data = pycgmIO.loadData('SampleData/Sample_2/RoboWalk.c3d')
+"""
+time = timeit.timeit(
+    '[decCGM(frame).pelvis_axis for frame in data]',
+    setup=setup,
+    number=NUM_RUNS
+)
+print("=======Decorator approach before modification=======")
+print(time)
+
+
+setup = """
+import pycgmIO
+from decorator import ModCGM as decModCGM
+
+data = pycgmIO.loadData('SampleData/Sample_2/RoboWalk.c3d')
+"""
+time = timeit.timeit(
+    '[decModCGM(frame).pelvis_axis for frame in data]',
+    setup=setup,
+    number=NUM_RUNS
+)
+print("=======Decorator approach after modification=======")
+print(time)
