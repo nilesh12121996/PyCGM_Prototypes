bf646286c0c940fc7c9304634da6eb42fc8cbb24
kellyhopkins672 <kelly.672.hopkins@gmail.com>
2021-10-12 22:50:46 -0400

    Adjusting to have original function & adding partial definition

--- prototypes/partials.py
+++ prototypes/partials.py
@@ -16,58 +16,120 @@ def markers(*args):
         return set_markers
     return decorator
 
+def partial(func, *part_args):
+    def wrapper(*extra_args):
+        markers = list(part_args)
+        markers.extend(extra_args)
+        return func(*markers)
 
-class CGM:
+    return wrapper
+
+
+class Gen_PJC_Func:
     def __init__(self, frame):
         self.frame = frame
         self._pelvis_axis = None
-
-    @markers('RASI', 'LASI', 'RPSI', 'LPSI', 'SACR')
-    def pelvisJointCenter(markers):
+    
+    def pelvisJointCenter(frame):
+        """Make the Pelvis Axis.
+        Takes in a dictionary of x,y,z positions and marker names, as well as an index
+        Calculates the pelvis joint center and axis and returns both.
+        Markers used: RASI, LASI, RPSI, LPSI
+        Other landmarks used: origin, sacrum
+        Pelvis X_axis: Computed with a Gram-Schmidt orthogonalization procedure [1]_ and then normalized.
+        Pelvis Y_axis: LASI-RASI x,y,z positions, then normalized.
+        Pelvis Z_axis: Cross product of x_axis and y_axis.
+        Parameters
+        ----------
+        frame : dict
+            Dictionaries of marker lists.
+        Returns
+        -------
+        pelvis : array
+            Returns an array that contains the pelvis origin in a 1x3 array of xyz values,
+            which is then followed by a [1x3, 3x3, 1x3] array composed of the 
+            pelvis x, y, z axis components, and the sacrum x,y,z position.
+        References
+        ----------
+        .. [1] M. P. Kadaba, H. K. Ramakrishnan, and M. E. Wootten, “Measurement of
+                lower extremity kinematics during level walking,” J. Orthop. Res.,
+                vol. 8, no. 3, pp. 383–392, May 1990, doi: 10.1002/jor.1100080310.
+        Examples
+        --------
+        >>> import numpy as np
+        >>> from .pyCGM import pelvisJointCenter
+        >>> frame = {'RASI': np.array([ 395.36,  428.09, 1036.82]),
+        ...          'LASI': np.array([ 183.18,  422.78, 1033.07]),
+        ...          'RPSI': np.array([ 341.41,  246.72, 1055.99]),
+        ...          'LPSI': np.array([ 255.79,  241.42, 1057.30]) }
+        >>> [arr.round(2) for arr in pelvisJointCenter(frame)] #doctest: +NORMALIZE_WHITESPACE
+        [array([ 289.27,  425.43, 1034.94]), array([[ 289.25,  426.43, 1034.83],
+        [ 288.27,  425.41, 1034.93],
+        [ 289.25,  425.55, 1035.94]]), array([ 298.6 ,  244.07, 1056.64])]
+        >>> frame = {'RASI': np.array([ 395.36,  428.09, 1036.82]),
+        ...          'LASI': np.array([ 183.18,  422.78, 1033.07]),
+        ...          'SACR': np.array([ 294.60,  242.07, 1049.64]) }
+        >>> [arr.round(2) for arr in pelvisJointCenter(frame)] #doctest: +NORMALIZE_WHITESPACE
+        [array([ 289.27,  425.43, 1034.94]), array([[ 289.25,  426.43, 1034.87],
+        [ 288.27,  425.41, 1034.93],
+        [ 289.25,  425.51, 1035.94]]), array([ 294.6 ,  242.07, 1049.64])]
+        """
         # Get the Pelvis Joint Centre
 
-        # REQUIRED MARKERS:
+        #REQUIRED MARKERS:
         # RASI
         # LASI
         # RPSI
         # LPSI
 
-        rasi, lasi, rpsi, lpsi, sacr = markers
+        RASI = frame['RASI']
+        LASI = frame['LASI']
 
-        if rpsi is not None and lpsi is not None:
-            sacrum = (rpsi + lpsi)/2.0
-        else:
-            sacrum = sacr
+        try:
+            RPSI = frame['RPSI']
+            LPSI = frame['LPSI']
+            #  If no sacrum, mean of posterior markers is used as the sacrum
+            sacrum = (RPSI+LPSI)/2.0
+        except:
+            pass #going to use sacrum marker
+
+        #  If no sacrum, mean of posterior markers is used as the sacrum
+        if 'SACR' in frame:
+            sacrum = frame['SACR']
 
         # REQUIRED LANDMARKS:
         # origin
         # sacrum
 
         # Origin is Midpoint between RASI and LASI
-        origin = (rasi+lasi)/2.0
+        origin = (RASI+LASI)/2.0
 
-        beta1 = origin - sacrum
-        beta2 = lasi - rasi
+        # This calculate the each axis
+        # beta1,2,3 is arbitrary name to help calculate.
+        beta1 = origin-sacrum
+        beta2 = LASI-RASI
 
         # Y_axis is normalized beta2
-        y_axis = beta2 / np.linalg.norm(beta2)
+        y_axis = beta2/norm3d(beta2)
 
-        beta3_cal = np.dot(beta1, y_axis)
-        beta3_cal2 = beta3_cal * y_axis
+        # X_axis computed with a Gram-Schmidt orthogonalization procedure(ref. Kadaba 1990)
+        # and then normalized.
+        beta3_cal = np.dot(beta1,y_axis)
+        beta3_cal2 = beta3_cal*y_axis
         beta3 = beta1-beta3_cal2
-        x_axis = beta3/np.linalg.norm(beta3)
+        x_axis = beta3/norm3d(beta3)
 
         # Z-axis is cross product of x_axis and y_axis.
-        z_axis = np.cross(x_axis, y_axis)
+        z_axis = cross(x_axis,y_axis)
 
         # Add the origin back to the vector
         y_axis = y_axis+origin
         z_axis = z_axis+origin
         x_axis = x_axis+origin
 
-        pelvis_axis = np.asarray([x_axis, y_axis, z_axis])
+        pelvis_axis = np.asarray([x_axis,y_axis,z_axis])
 
-        pelvis = [origin, pelvis_axis, sacrum]
+        pelvis = [origin,pelvis_axis,sacrum] #probably don't need to return sacrum
 
         return pelvis
 
@@ -82,52 +144,106 @@ class CGM:
         return self._pelvis_axis
 
 
-class ModCGM(CGM):
-    @markers('RASI', 'LASI', 'RPSI', 'LPSI')
-    def pelvisJointCenter(markers):
+class Mod_PJC_Func(CGM):
+    def pelvisJointCenter(frame):
+        """Make the Pelvis Axis.
+        Takes in a dictionary of x,y,z positions and marker names, as well as an index
+        Calculates the pelvis joint center and axis and returns both.
+        Markers used: RASI, LASI, RPSI, LPSI
+        Other landmarks used: origin, sacrum
+        Pelvis X_axis: Computed with a Gram-Schmidt orthogonalization procedure [1]_ and then normalized.
+        Pelvis Y_axis: LASI-RASI x,y,z positions, then normalized.
+        Pelvis Z_axis: Cross product of x_axis and y_axis.
+        Parameters
+        ----------
+        frame : dict
+            Dictionaries of marker lists.
+        Returns
+        -------
+        pelvis : array
+            Returns an array that contains the pelvis origin in a 1x3 array of xyz values,
+            which is then followed by a [1x3, 3x3, 1x3] array composed of the 
+            pelvis x, y, z axis components, and the sacrum x,y,z position.
+        References
+        ----------
+        .. [1] M. P. Kadaba, H. K. Ramakrishnan, and M. E. Wootten, “Measurement of
+                lower extremity kinematics during level walking,” J. Orthop. Res.,
+                vol. 8, no. 3, pp. 383–392, May 1990, doi: 10.1002/jor.1100080310.
+        Examples
+        --------
+        >>> import numpy as np
+        >>> from .pyCGM import pelvisJointCenter
+        >>> frame = {'RASI': np.array([ 395.36,  428.09, 1036.82]),
+        ...          'LASI': np.array([ 183.18,  422.78, 1033.07]),
+        ...          'RPSI': np.array([ 341.41,  246.72, 1055.99]),
+        ...          'LPSI': np.array([ 255.79,  241.42, 1057.30]) }
+        >>> [arr.round(2) for arr in pelvisJointCenter(frame)] #doctest: +NORMALIZE_WHITESPACE
+        [array([ 289.27,  425.43, 1034.94]), array([[ 289.25,  426.43, 1034.83],
+        [ 288.27,  425.41, 1034.93],
+        [ 289.25,  425.55, 1035.94]]), array([ 298.6 ,  244.07, 1056.64])]
+        >>> frame = {'RASI': np.array([ 395.36,  428.09, 1036.82]),
+        ...          'LASI': np.array([ 183.18,  422.78, 1033.07]),
+        ...          'SACR': np.array([ 294.60,  242.07, 1049.64]) }
+        >>> [arr.round(2) for arr in pelvisJointCenter(frame)] #doctest: +NORMALIZE_WHITESPACE
+        [array([ 289.27,  425.43, 1034.94]), array([[ 289.25,  426.43, 1034.87],
+        [ 288.27,  425.41, 1034.93],
+        [ 289.25,  425.51, 1035.94]]), array([ 294.6 ,  242.07, 1049.64])]
+        """
         # Get the Pelvis Joint Centre
 
-        # REQUIRED MARKERS:
+        #REQUIRED MARKERS:
         # RASI
         # LASI
         # RPSI
         # LPSI
 
-        rasi, lasi, rpsi, lpsi = markers
+        RASI = frame['RASI']
+        LASI = frame['LASI']
 
-        if rpsi is not None and lpsi is not None:
-            sacrum = (rpsi + lpsi)/2.0
-        else:
-            sacrum = 1
+        try:
+            RPSI = frame['RPSI']
+            LPSI = frame['LPSI']
+            #  If no sacrum, mean of posterior markers is used as the sacrum
+            sacrum = (RPSI+LPSI)/2.0
+        except:
+            pass #going to use sacrum marker
+
+        #  If no sacrum, mean of posterior markers is used as the sacrum
+        if 'SACR' in frame:
+            sacrum = frame['SACR']
 
         # REQUIRED LANDMARKS:
         # origin
         # sacrum
 
         # Origin is Midpoint between RASI and LASI
-        origin = (rasi+lasi)/2.0
+        origin = (RASI+LASI)/2.0
 
-        beta1 = origin - sacrum
-        beta2 = lasi - rasi
+        # This calculate the each axis
+        # beta1,2,3 is arbitrary name to help calculate.
+        beta1 = origin-sacrum
+        beta2 = LASI-RASI
 
         # Y_axis is normalized beta2
-        y_axis = beta2 / np.linalg.norm(beta2)
+        y_axis = beta2/norm3d(beta2)
 
-        beta3_cal = np.dot(beta1, y_axis)
-        beta3_cal2 = beta3_cal * y_axis
+        # X_axis computed with a Gram-Schmidt orthogonalization procedure(ref. Kadaba 1990)
+        # and then normalized.
+        beta3_cal = np.dot(beta1,y_axis)
+        beta3_cal2 = beta3_cal*y_axis
         beta3 = beta1-beta3_cal2
-        x_axis = beta3/np.linalg.norm(beta3)
+        x_axis = beta3/norm3d(beta3)
 
         # Z-axis is cross product of x_axis and y_axis.
-        z_axis = np.cross(x_axis, y_axis)
+        z_axis = cross(x_axis,y_axis)
 
         # Add the origin back to the vector
         y_axis = y_axis+origin
         z_axis = z_axis+origin
         x_axis = x_axis+origin
 
-        pelvis_axis = np.asarray([x_axis, y_axis, z_axis])
+        pelvis_axis = np.asarray([x_axis,y_axis,z_axis])
 
-        pelvis = [origin, pelvis_axis, sacrum]
+        pelvis = [origin,pelvis_axis,sacrum] #probably don't need to return sacrum
 
         return pelvis
